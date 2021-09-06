20735a6b8f30ad4aca5067650a141559d5194140
Rahul Patel <rahulxyzpatel@icloud.com>
2021-09-06 13:53:18 +1200

    Add axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=nzd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        // console.log(res.data);
+      })
+      .catch((error) => alert(error));
+  }, []);
   return (
-    <div className="App">
-      <h1>APII</h1>
+    <div className="crypto-coin-app">
+      <div className="crypto-coin-search">
+        <h1 className="crypto-coin-text">Search a Crypto Currency</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            className="crypto-coin-input"
+          />
+        </form>
+      </div>
     </div>
   );
 }
